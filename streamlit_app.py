9dbdaf86d5002b042339875374b380c996777615
mounikaparuchuri <57961127+mounikaparuchuri@users.noreply.github.com>
2025-09-07 00:33:22 +0000

    69

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,7 +177,7 @@ if prompt_input and prompt_input.text:
             model_messages.append({"role": role, "parts": parts})
         for msgStr in model_messages:
             for mstr in msgStr.items():
-                print("model_messages " + {mstr}) 
+                print("model_messages " + mstr) 
         # # The system prompt is an initial instruction and not part of the conversation turn.
         # # You handle this by adding the instruction to the first user message.
         # if model_messages and st.session_state.messages[0]["role"] == "system":
