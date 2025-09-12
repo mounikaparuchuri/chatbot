4abf9f022f046557bbf7173dbdc9fc71703a15bf
mounikaparuchuri <57961127+mounikaparuchuri@users.noreply.github.com>
2025-09-12 16:25:55 +0000

    4

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ else:
 # This ensures a continuous chat history with the system instruction.
 if "chat" not in st.session_state:
     #model = genai.GenerativeModel("gemini-1.5-pro-001")
-    model = genai.GenerativeModel(model="gemini-1.5-pro-001",
+    model = genai.GenerativeModel("gemini-1.5-pro-001",
     system_instructions = {
         "role": "system",
         "content": system_prompt
